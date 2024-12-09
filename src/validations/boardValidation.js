36ad847377fe5415dc4ce53097260127254b6680
Huy Vo <vominhhuy0911@gmail.com>
2024-12-09 20:55:17 +0700

    config validation with joi

---
+++ src/validations/boardValidation.js
@@ -0,0 +1,44 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => {
+  const schema = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
+      'any.required': 'Title is required',
+      'string.empty': 'Title is not allowed to be empty',
+      'string.min': 'Title must be at least 3 characters',
+      'string.max': 'Title must be at most 50 characters',
+      'string.trim': 'Title must not have leading or trailing whitespace'
+    }),
+    description: Joi.string()
+      .required()
+      .min(3)
+      .max(255)
+      .trim()
+      .strict()
+      .messages({
+        'any.required': 'Description is required',
+        'string.empty': 'Description is not allowed to be empty',
+        'string.min': 'Description must be at least 3 characters',
+        'string.max': 'Description must be at most 255 characters',
+        'string.trim':
+          'Description must not have leading or trailing whitespace'
+      })
+  })
+
+  try {
+    await schema.validateAsync(req.body, { abortEarly: false })
+    // next()
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: 'POST: API create new board' })
+  } catch (error) {
+    res
+      .status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ errors: new Error(error).message })
+  }
+}
+
+export const boardValidation = {
+  createNew
+}
