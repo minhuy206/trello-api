270b696879459dfe425bab486b451865712fe9f9
Huy Vo <vominhhuy0911@gmail.com>
2024-12-09 21:17:32 +0700

    config controller handling

---
+++ src/controllers/boardController.js
@@ -0,0 +1,17 @@
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => {
+  try {
+    console.log('req.body:', req.body)
+
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: 'POST from Controller: API create new board' })
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message })
+  }
+}
+
+export const boardController = {
+  createNew
+}
