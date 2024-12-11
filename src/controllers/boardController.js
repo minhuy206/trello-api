2bfafa9919c2e978ec0aceda9137916f77db6d5b
Huy Vo <vominhhuy0911@gmail.com>
2024-12-11 13:45:06 +0700

    complete api get board

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -13,6 +13,19 @@ const createNew = async (req, res, next) => {
   }
 }
 
+const getBoard = async (req, res, next) => {
+  try {
+    const boardId = req.params.id
+
+    const board = await boardService.getBoard(boardId)
+
+    return res.status(StatusCodes.OK).json(board)
+  } catch (error) {
+    next(error)
+  }
+}
+
 export const boardController = {
-  createNew
+  createNew,
+  getBoard
 }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatter'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (data) => {
   try {
@@ -15,6 +17,20 @@ const createNew = async (data) => {
   }
 }
 
+const getBoard = async (boardId) => {
+  try {
+    const board = await boardModel.getBoard(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found')
+    }
+
+    return board
+  } catch (error) {
+    throw error
+  }
+}
+
 export const boardService = {
-  createNew
+  createNew,
+  getBoard
 }
