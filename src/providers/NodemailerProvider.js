a4c1dfbdb98585956b431e7829e20ce63d0043ab
Huy Vo <vominhhuy0911@gmail.com>
2024-12-26 14:02:05 +0700

    config env variables for nodemailer provider

--- src/providers/NodemailerProvider.js
+++ src/providers/NodemailerProvider.js
@@ -2,21 +2,21 @@ import { env } from '~/config/environment'
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
+  host: env.SMTP_SERVER,
+  port: env.SMTP_PORT,
   secure: false, // true for port 465, false for other ports
   auth: {
-    user: 'vominhhuy0911@gmail.com',
-    pass: 'hhmboydxssypvoye'
+    user: env.SMTP_USERNAME,
+    pass: env.SMTP_PASSWORD
   }
 })
 
 let sendEmail = async (recipientEmail, customSubject, htmlContent) => {
   return await transporter.sendMail({
-    from: 'minhuy', // sender address
-    to: recipientEmail, // list of receivers
-    subject: customSubject, // Subject line
-    html: htmlContent // html body
+    from: '"minhuy" <vominhhuy0911@gmail.com>',
+    to: recipientEmail,
+    subject: customSubject,
+    html: htmlContent
   })
 }
 
