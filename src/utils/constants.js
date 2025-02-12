d6f1544161c56d619650fc82c57af11a3002747d
Huy Vo <forwork@minhuy.dev>
2025-02-12 18:30:48 +0700

    update whitelist domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 import { env } from '~/config/environment'
-export const WHITELIST_DOMAINS = ['https://trello.minhuy.dev/']
+export const WHITELIST_DOMAINS = ['https://trello.minhuy.dev']
 
 export const BOARD_TYPES = {
   PUBLIC: 'public',
